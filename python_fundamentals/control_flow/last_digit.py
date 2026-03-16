8f97b9578407663cfd9f70b3da6b07fec205f2f0
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-03-16 13:11:08 +0100

    task 1

---
+++ python_fundamentals/control_flow/last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+number = __import__('random').randint(-10000, 10000)
+
+if number >= 0:
+    r = number % 10
+else:
+    r = number % -10
+if r > 5:
+    m = "and is greater than 5"
+elif r == 0:
+    m = "and is 0"
+elif r < 6 and r != 0:
+    m = "and is less than 6 and not 0"
+print("Last digit of {} is {} {}".format(number, r, m))
