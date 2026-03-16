6d28118f7b04b935b5441dcf6ccb29f075d6cddb
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-03-16 15:58:09 +0100

    task 5

--- python_fundamentals/functions_modules/add_0.py
+++ python_fundamentals/functions_modules/add_0.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
 
-def add(a, b):
-    return a + b
+from add import add
+
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    r = add(a, b)
+    print(f'{a} + {b} = {r}')
