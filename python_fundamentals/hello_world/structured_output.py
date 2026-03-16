770079d366df39b628ef57dd5689c3b60a5a5045
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-03-16 12:42:52 +0100

    task 1 done

---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+n = 3.14159265
+
+language = "Python"
+
+print(f'Language: {language}')
+print(f'Version: 3')
+print(f'Pi approx: {n:.2f}')
+print(f'Computation valid: {7 > 6}')
