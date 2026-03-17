6bbcc932dafa3364fee2ee8afdf41c4b793cc6dd
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-03-17 12:10:30 +0100

    task 5 update

--- python_fundamentals/functions_modules/add_0.py
+++ python_fundamentals/functions_modules/add_0.py
@@ -3,9 +3,9 @@
 
 from add_0 import add
 
+a = 1
+b = 2
 
 if __name__ == "__main__":
-    a = 1
-    b = 2
-    r = add(a, b)
-    print("{0} + {1} = {2}".format(a, b, r))
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
