dc04eeabb792d3923482bbb48003d97ecd694169
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-03-19 10:46:30 +0100

    task 3 done

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        ln = len(row)
+        for i in range(ln):
+            print("{:d}".format(row[i]), end=" " if i < ln - 1 else "\n")
