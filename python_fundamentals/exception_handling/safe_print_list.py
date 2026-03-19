7cf03c3f1d35979667612c39c2ac09f95757b554
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-03-19 17:31:59 +0100

    task 0 init file

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 
+def safe_print_list(my_list=[], x=0):
+    max = x
+    if x > len(my_list):
+        max = len(my_list)
+    for i in range(max):
+        print(my_list[i])
