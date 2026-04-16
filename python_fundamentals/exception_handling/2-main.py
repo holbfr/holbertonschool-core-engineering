3b774ec7f8a368466a45c44bf4569b0984fa02e7
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-16 15:09:59 +0200

    task 2 done

---
+++ python_fundamentals/exception_handling/2-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+
+from safe_print_list_integers import safe_print_list_integers
+
--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    while count < x:
+        try:
+            if type(my_list[count]) is int:
+                print(my_list[count])
+            count += 1
+        except IndexError:
+            break
+    return count
