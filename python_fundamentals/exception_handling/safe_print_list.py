1d4cd7235c9bb7298e7f71b45bdd89ef4f990cd4
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-16 12:10:42 +0200

    task 0 done

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 def safe_print_list(my_list=[], x=0):
-    max = x
-    if x > len(my_list):
-        max = len(my_list)
-    for i in range(max):
-        print(my_list[i])
+    count = 0
+    for i in range(my_list):
+        try:
+            print(my_list[i])
+            count += 1
+        except IndexError:
+            pass
+    return count
