66a6cfb3b5748cdf8421ebb0b94c32c609b74a12
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-16 16:39:53 +0200

    task 3 done

---
+++ python_fundamentals/exception_handling/3-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from safe_print_division import safe_print_division
+
+
+safe_print_division(10, 5)
+
+safe_print_division(6, 3)
+
+safe_print_division(5, 1)
+
+safe_print_division(10, 10)
+
+safe_print_division(5, 0)
+
+safe_print_division(22, -2)
--- python_fundamentals/exception_handling/safe_print_division.py
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        print("division by 0")
+    except Exception:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
