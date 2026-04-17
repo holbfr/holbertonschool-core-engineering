24f67651422a07528af8181d4cc92e90b0f85236
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-17 18:34:44 +0200

    task 7 update

--- python_oop/classes_and_object_model/1-rectangle.py
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Constructor of the Rectangle class """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
--- python_oop/classes_and_object_model/7-main.py
+++ python_oop/classes_and_object_model/7-main.py
@@ -7,3 +7,15 @@ print(my_rectangle.__dict__)
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
+
+
+try:
+    my_rectangle = Rectangle(2, -3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+
+try:
+    my_rectangle = Rectangle(-2, 3)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
