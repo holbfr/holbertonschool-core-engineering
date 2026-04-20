18bfda37ca062b589080e5752a5d30afab8b4d9f
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-20 17:55:37 +0200

    task 2 update output display

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -8,10 +8,11 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             el = my_list[i]
             if isinstance(el, int):
-                print("{:d}".format(el))
+                print("{:d}".format(el), end='')
                 num_printed_ints += 1
         except IndexError:
             break
         finally:
             i += 1
+    print()
     return num_printed_ints
