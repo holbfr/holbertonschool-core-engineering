6e0f4112ed40fd2e32dc769535773b5b98ea2abb
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-20 17:51:20 +0200

    update task 0 output display

--- python_fundamentals/exception_handling/safe_print_list.py
+++ python_fundamentals/exception_handling/safe_print_list.py
@@ -4,8 +4,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     while count < x:
         try:
-            print(my_list[count])
+            print(my_list[count], end='')
             count += 1
         except IndexError:
             break
+    print()
     return count
