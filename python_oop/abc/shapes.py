337d7857176a3c9f48527de69c841f6446889d66
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-21 18:19:27 +0200

    task 1

--- python_oop/abc/shapes.py
+++ python_oop/abc/shapes.py
@@ -2,5 +2,56 @@
 """
 Simple module for task validation
 """
+from abc import ABC, abstractmethod
+import math
 
 
+class Shape(ABC):
+    """Shape ABC definition"""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """Circle class definition"""
+    def __init__(self, radius):
+        """Circle class constructor"""
+        self.radius = radius
+
+    def area(self):
+        """Circle area value"""
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        """Circle perimeter value"""
+        return math.pi * self.radius * 2
+
+
+class Rectangle(Shape):
+    """Rectangle class definition"""
+    def __init__(self, width, height):
+        """Rectangle class constructor"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Rectangle area value"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Rectangle perimeter value"""
+        return 2 * (self.height + self.width)
+
+
+def shape_info(shape):
+    """Prints shape area and perimeter values"""
+    area = shape.area()
+    perimeter = shape.perimeter()
+    print("Area: {}".format(area))
+    print("Perimeter: {}".format(perimeter))
