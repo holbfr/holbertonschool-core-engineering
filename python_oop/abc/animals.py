e98bae461e20e6cce6dbfbabaa54de45c36b0d9b
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-21 18:03:42 +0200

    task 0 done

--- python_oop/abc/animals.py
+++ python_oop/abc/animals.py
@@ -2,5 +2,30 @@
 """
 Simple module for task validation
 """
+from abc import ABC, abstractmethod
 
+
+class Animal(ABC):
+    """Abstract animal class definition"""
+
+    @abstractmethod
+    def sound(self):
+        """Abstract sound method"""
+        pass
+
+
+class Dog(Animal):
+    """ Dog class definition """
+
+    def sound(self):
+        """Sound method for Dog class"""
+        return "Bark"
+
+
+class Cat(Animal):
+    """ Cat class definition """
+
+    def sound(self):
+        """Sound method for Cat class"""
+        return "Meow"
 
