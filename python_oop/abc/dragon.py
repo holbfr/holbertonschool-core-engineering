f3262d59b1fa8b20db96f418b4a492db77a7fc9b
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-22 12:03:00 +0200

    task 3 done

--- python_oop/abc/dragon.py
+++ python_oop/abc/dragon.py
@@ -4,3 +4,22 @@ Simple module for task validation
 """
 
 
+class SwinMixin():
+    """SwinMixin class definition"""
+    def swim(self):
+        """Prints the creature swims"""
+        print("The creature swims!")
+
+
+class FlyMixin():
+    """FlyMixin class definition"""
+    def fly(self):
+        """Prints the creature can fly"""
+        print("The creature flies!")
+
+
+class Dragon(SwinMixin, FlyMixin):
+    """Dragon class definition"""
+    def roar(self):
+        """Prints the creature roaring"""
+        print("The dragon roars!")
