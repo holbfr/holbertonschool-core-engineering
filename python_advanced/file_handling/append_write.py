a3f9dda05ee72c5f7edcc4a60fbb4ede91d880d4
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-23 15:21:17 +0200

    task 2 done

--- python_advanced/file_handling/append_write.py
+++ python_advanced/file_handling/append_write.py
@@ -4,3 +4,8 @@ Simple Python module for task validation
 """
 
 
+def append_write(filename="", text=""):
+    """Append text to a file and returns the number of added characters"""
+    with open(filename, mode='a', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
