aa81e43eb1451140a49a7d6ff234c205e971f469
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-07-22 15:54:52 +0200

    task 1 update websockets client from sync to async

--- python_advanced/websockets/ws_client.py
+++ python_advanced/websockets/ws_client.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 
 import asyncio
-from websockets.sync.client import connect
+from websockets.asyncio.client import connect
 
 
 async def connect_and_send(uri: str, text: str) -> str:
-    with connect(uri) as websocket:
+    async with connect(uri) as websocket:
         user_msg = "Hello WebSocket"
-        websocket.send(user_msg)
-        print(user_msg)
-        server_resp = websocket.recv()
-        print(server_resp, end="")
-    websocket.close()
+        await websocket.send(user_msg)
+        await websocket.recv()
+        print(text, end="")
+    await websocket.close()
     return text
 
 
-async def main():
-    uri = "ws://localhost:8765"
-    await connect_and_send(uri, "demo")
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    uri = "ws://localhost:8765"
+    asyncio.run(connect_and_send(uri=uri, text="demo"))
