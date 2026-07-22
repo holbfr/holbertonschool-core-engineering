7f46ce2306d50c61486722b71b5e17fe1de97513
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-07-22 12:54:46 +0200

    [fix] echo_server.py pycodestyle issues

--- python_advanced/websockets/echo_server.py
+++ python_advanced/websockets/echo_server.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 """
-An async connection_handler(websocket) that, for each text message received on that connection, sends back one outgoing text message whose value is exactly the same message string (no extra newline unless the client sent one).
+An async connection_handler(websocket) that, for each text message received
+on that connection, sends back one outgoing text message whose value is
+exactly the same message string (no extra newline unless the client sent one).
 
 Constraints
 Use the websockets library
 Use asynchronous programming (async / await)
-The server must handle messages continuously (it must not stop after one message)
+The server must handle messages continuously (it must not
+stop after one message).
 Messages must be processed as they arrive (no batching or delays)
 Only text messages need to be supported
 The server must start when executing:
 python3 echo_server.py
+After launching your app server you can test the connection between
+the server and the client on another terminal using the following command:
+websockets ws://localhost:8765/
 """
 
 import asyncio
@@ -28,7 +34,7 @@ async def connection_handler(websocket):
     response = message
     await websocket.send(response)
     print(response)
-    
+
 
 async def main():
     async with serve(connection_handler, "localhost", 8765) as server:
